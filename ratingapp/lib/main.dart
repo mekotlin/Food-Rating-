3750cc61a8f9a142ce82659409459ba7899467c7
mekoltin <dnyaneshwar@onevoicetransmedia.com>
2020-06-25 18:01:41 +0530

    Orientation Changes

--- ratingapp/lib/main.dart
+++ ratingapp/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ratingapp/home_page.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
   runApp(MyApp());
 }
 
